8be4144bfb7355b5f43a701b2963ae5f70a8e905
Francis James Tolentino <francistolentino1107@gmail.com>
2024-03-19 00:39:48 +0800

    sus

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(const MyApp());
 }
 
+// HELLO WORLD
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -11,60 +12,114 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Video Tutorial',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePageHelloWorld(title: 'Flutter Video'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+// SCAFFOLD
+// TEXT - TEXT STYLES
+// FLOATING ACTION BUTTON
+// CENTER
+// APP BAR
+// OPTIONAL BOTTOM NAVIGATION BAR
+// class HomePageHelloWorld extends StatelessWidget {
+//   const HomePageHelloWorld({super.key, required this.title});
+//
+//   final String title;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//         elevation: 10,
+//         title: Text(title),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {},
+//         child: const Icon(Icons.add),
+//       ),
+//       bottomNavigationBar: BottomNavigationBar(
+//         items: const [
+//           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+//           BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: "Cart"),
+//           BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings"),
+//         ],
+//       ),
+//       // BODY NA UNA
+//       body: const Center(
+//         child: Text("Hello World", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),),
+//       ),
+//       // MORE BODY!!
+//       // body: const SizedBox(
+//       //   height: double.maxFinite,
+//       //   width: double.maxFinite,
+//       //   child: Column(
+//       //     mainAxisAlignment: MainAxisAlignment.center, // DIFFERENT MAIN AXIS VALUES
+//       //     crossAxisAlignment: CrossAxisAlignment.center, //
+//       //     children: [
+//       //       Text("Xyrone Jei Sanchez", style: TextStyle(fontSize: 25),),
+//       //       Text("Gilbert Sicat", style: TextStyle(fontSize: 23)),
+//       //       Text("Mark Daniel Soriano", style: TextStyle(fontSize: 21)),
+//       //       Text("Francis James Tolentino", style: TextStyle(fontSize: 19)),
+//       //       Text("Ian Andrew Yango", style: TextStyle(fontSize: 17)),
+//       //     ],
+//       //   )
+//       // )
+//     );
+//   }
+// }
+
+class HomePageHelloWorld extends StatefulWidget {
+  const HomePageHelloWorld({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePageHelloWorld> createState() => _HomePageHelloWorldState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageHelloWorldState extends State<HomePageHelloWorld> {
 
-  void _incrementCounter() {
+  final _names = ["Xyrone Jei Sanchez", "Gilbert Sicat", "Mark Daniel Soriano", "Francis James Tolentino", "Ian Andrew Yango"];
+
+  void randomizeNames() {
     setState(() {
-      _counter++;
+      _names.shuffle();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return  Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        elevation: 10,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: randomizeNames,
+        label: const Text("Randomize"),
+        icon: const Icon(Icons.refresh),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // BODY NA UNA
+      // MORE BODY!!
+      body: SizedBox(
+        height: double.maxFinite,
+        width: double.maxFinite,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // DIFFERENT MAIN AXIS VALUES
+          crossAxisAlignment: CrossAxisAlignment.center, //
+          children: _names.map((entry) => Text(entry, style: const TextStyle(fontSize: 18),)).toList(),
+        )
+      )
     );
   }
 }
